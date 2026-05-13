6ac1a922e5aef1de9bad397ae60da071658615f8
patilarnavr-hue <patilarnavr@gmail.com>
2026-05-13 22:26:01 +0530

    Update Capacitor configuration for iOS app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,20 +1,9 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.038170b3b0c546e7845fa278e33dab15',
-  appName: 'AgroEye',
-  webDir: 'dist',
-  server: {
-    url: 'https://038170b3-b0c5-46e7-845f-a278e33dab15.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
-  },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 2000,
-      backgroundColor: "#F5F5DC",
-      showSpinner: false,
-    },
-  },
+  appId: 'com.ios.app',
+  appName: 'agroeye_28_ios_v1.0',
+  webDir: 'dist'
 };
 
 export default config;
